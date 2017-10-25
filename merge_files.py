2ce634417fe1f0695c2efe554a7651b8c65226f9
ulises-1229 <ulises.1229@gmail.com>
2017-10-25 01:34:15 -0500

    new folder structure: generate  CSV

--- merge_files.py
+++ merge_files.py
@@ -1,6 +1,6 @@
 import csv
-from os import walk
 import os
+from collections import defaultdict
 
 path = os.getcwd()
 
@@ -8,6 +8,14 @@ path = os.getcwd()
 for subdir, dirs, files in os.walk('./data'):
     print "All the data files are: " + str(files)
 
+columns = defaultdict(list)
 
-
+for file in files:
+    with open(path+"/data/"+file) as f:
+        reader = csv.DictReader(f)  # read rows into a dictionary format
+        for row in reader:  # read a row as {column1: value1, column2: value2,...}
+            for (k, v) in row.items():  # go over each column name and value
+                columns[k].append(v)  # append the value into the appropriate list
+                print columns[k]
+                # based on column name k
 #with open()
