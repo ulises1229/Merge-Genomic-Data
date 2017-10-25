7f927ed6a0049a41d446e52c7421d8feb4d57d39
ulises-1229 <ulises.1229@gmail.com>
2017-10-25 00:15:01 -0500

    new dir structure

--- merge_files.py
+++ merge_files.py
@@ -1,5 +1,14 @@
 import csv
+from os import walk
+import os
+
+path = os.getcwd()
+
+# Read all the files of current directory
+files = []
+for subdir, dirs, files in os.walk('./data'):
+    for file in files:
+      print file
 
 
-
-with open()
+#with open()
