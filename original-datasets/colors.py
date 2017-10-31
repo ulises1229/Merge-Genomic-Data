6f31c3ec9e18e199d4c90f39361761b00a06d490
ulises-1229 <ulises.1229@gmail.com>
2017-10-30 20:05:08 -0600

    Fixed importation

--- original-datasets/colors.py
+++ original-datasets/colors.py
@@ -1,6 +1,5 @@
 import xlrd
 import csv
-from unidecode import unidecode
 
 def csv_from_excel():
     wb = xlrd.open_workbook('FS_01_DOWN_COLORS.xlsx')
@@ -9,10 +8,7 @@ def csv_from_excel():
     wr = csv.writer(your_csv_file, quoting=csv.QUOTE_ALL)
 
     for rownum in range(sh.nrows):
-    	text = unidecode(unicode(sh.row_values(rownum), encoding = "utf-8"))
-    	print text
-    	wr.writerow(text)
-
+    	wr.writerow([unicode(c).encode('utf8') for c in sh.row_values(rownum)])
     your_csv_file.close()
 
 # runs the csv_from_excel function:
