bdaa55334a521a0c3e8d451bfc31c622a9800c1a
Ulises Olivares <ulises.1229@gmail.com>
2018-09-03 20:34:10 -0500

    Runs and modification

--- merge_files.py
+++ merge_files.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-__author__ = 'Ulises Olivares - uolivares@enesmorelia.unam.mx'
+#__author__ = 'Ulises Olivares - uolivares@enesmorelia.unam.mx
 
-import csv
 import os
 from collections import defaultdict
-
-# Import for preprocessing files
-#import xlrd
 import csv
 
 
@@ -29,13 +25,45 @@ def findElement(annotations, element):
     :param element:
     :return:
     '''
-    element = element.strip()
+
+    orig = element
+
+    # Find the | char to just obtain the first part of the ID
+    pos = element.find("|")
+    pos2 = element.find(" ")
+
+
+
+    if pos2 < pos and pos2 != -1:
+        element = element[0:pos2]
+        # Remove empty spaces
+        element = element.replace(" ", "")
+    else:
+        # Cut string when the char | is present
+        element = element[0:pos]
+        # Remove empty spaces
+        element = element.replace(" ", "")
+
+
+
+    test = next((s for s in annotations.keys() if element in s), None)  # returns 'abc123'
+
+    if test == None:
+        print "Early Not found "
+
     if element in annotations.keys():
         return annotations[element]
     else:
         return "not found"
 
 def readAnnotFile(dataPath, file):
+    '''
+
+    :param dataPath:
+    :param file:
+    :return:
+    '''
+
     annotations = {}
     with open(dataPath + file) as csvfile:
         reader = csv.reader(csvfile)
@@ -51,14 +79,85 @@ def readAnnotFile(dataPath, file):
                             annotations[element] = row[3].strip()  # Store test seqs as key and name as value
                             element = ''
                 else: # Only one element is in the cell
-                    annotations[row[1]] = row[3].strip()
+
+                    # FIXME: MAKE THIS A METHOD IT IS USED MORE THAN ONCE
+                    # Remove empty spaces
+                    key =  row[1].replace(" ","")
+
+                    # Find the | char to just obtain the first part of the ID
+                    pos = key.find("|")
+
+                    # Cut string when the char | is present
+                    key = key[0:pos]
+
+                    # Cut find the
+
+                    annotations[key] = row[3].replace(" ","")
             else:
-                print "error no valid register on line"
+                print "error, not a valid register" + row[0]
     #print annotations
     return annotations
 
+def readColorsFile(dataPath, file, annotations):
+    '''
+
+    :param dataPath:
+    :param file:
+    :param annotations:
+    :return:
+    '''
+    colorRegisters = {}
+
+    with open(dataPath + file) as csvfile:
+        reader = csv.reader(csvfile)
+        count = 0
+        annotatedFile = file[0:file.find(".csv")] + "_ANNOT.csv"
+        with open(dataPath + annotatedFile, 'wb') as csvfile:
+            writer = csv.writer(csvfile)
+            for row in reader:
+                if count != 0:  # Ignore first Line
+                    colorRegisters[row[0]] = defaultdict(list)
+                    element = ''
+                    # Write new file with annotations
+                    csvRow = list()
+                    # Append first row
+                    csvRow.append(row[0])
+                    # Append second row
+                    csvRow.append(row[1])
+
+                    for i in row[2]:  # Iterate over test seqs and replace it for the long names
+                        if i != ',':  # split the line once a comma is detected
+                            element = element + i
+                        else:
+                            element = element.strip()  # Remove spaces at the beginning and at the end
+                            anotName = findElement(annotations, element)
+                            if anotName != 'not found':
+                                if anotName == '#NAME?':
+                                    print element
+                                print anotName
+                                # print str(i)+ ": " + str(anotName)
+                                colorRegisters[row[0]][row[1]].append(anotName)
+                                # TODO: write the information in a CSV file
+                                csvRow.append(anotName)
+                                #print "element found"
+                            else:
+                                print "Register " + element + " not found"
+                                # print element
+                            element = ''  # reset element to an empty value
+                        count = count + 1
+                        #break
+                    writer.writerow(csvRow)
+                else:  # Just increment count for the first line
+                    count = count + 1
+        csvfile.close()
+    return colorRegisters
+
 
 def main():
+    '''
+
+    :return:
+    '''
 
     path = os.getcwd()
     dataPath = path + "/data/"
@@ -71,36 +170,13 @@ def main():
     colorRegisters = {}
     annotations = {}
 
-    # FIXME: ALWAYS READ FIRST ANNOT FILE
+
+    # SORT all files to read first the ANOTT file
+    files.sort()
     for file in files:
-        # FIXME: PUT THIS INTO A METHOD
         # Read colors file
         if "COLORS" in file:
-            count = 0
-            with open(dataPath + file) as csvfile:
-                reader = csv.reader(csvfile)
-                for row in reader:
-                    if count != 0: # Ignore first Line
-                        colorRegisters[row[0]] = defaultdict(list)
-                        element = ''
-                        for i in row[2]: # Iterate over test seqs and replace it for the long names
-                            if i != ',': # split the line once a comma is detected
-                                element = element + i
-                            else:
-                                element = element.strip() # Remove spaces at the beginning and at the end
-                                anotName = findElement(annotations, element)
-                                if anotName != 'not found':
-                                    #print str(i)+ ": " + str(anotName)
-                                    colorRegisters[row[0]][row[1]].append(anotName)
-                                    # TODO: write the information in a CSV file
-                                else:
-                                    print "Register " + element + " not found"
-                                    #print element
-                                element = '' # reset element to an empty
-                        count = count + 1
-                        break
-                    else:  # Just increment count for the first line
-                        count = count + 1
+            colorsRegisters = readColorsFile(dataPath, file, annotations)
 
 
         # Read annot file
